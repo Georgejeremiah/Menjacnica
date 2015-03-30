06a50d44b5ce3e6fc60b14794cd169efc25228d1
Georgejeremiah <djolekg93@hotmail.com>
2015-03-30 20:25:25 +0200

    override-ovane su metode equals,hashcode i toString

--- src/valuta/valuta.java
+++ src/valuta/valuta.java
@@ -9,6 +9,11 @@ private GregorianCalendar datumValute;
 private int prodajni;
 private int kupovni;
 private String srednji;
+public void setDatumValute(int dan,int Mesec,int Godina){
+	datumValute.set(Godina, Mesec-1, dan);}
+public GregorianCalendar getDatumValute(){
+	return datumValute;
+	}
 public String getNaziv() {
 	return naziv;
 }
@@ -20,12 +25,6 @@ public String getSkraceniNaziv() {
 }
 public void setSkraceniNaziv(String skraceniNaziv) {
 	this.skraceniNaziv = skraceniNaziv;
-}
-public GregorianCalendar getDatumValute() {
-	return datumValute;
-}
-public void setDatumValute(GregorianCalendar datumValute) {
-	this.datumValute = datumValute;
 }
 public int getProdajni() {
 	return prodajni;
@@ -45,6 +44,58 @@ public String getSrednji() {
 public void setSrednji(String srednji) {
 	this.srednji = srednji;
 }
-
+@Override
+	public String toString() {
+		return "Naziv valute: "+naziv+" prodajni kurs: "+prodajni+" kupovni kurs: "+kupovni+" srednji kurs: "+srednji+" dan valute: "+datumValute.get(GregorianCalendar.DAY_OF_WEEK)+" mesec valute: "+datumValute.get(GregorianCalendar.MONTH)+"godina valute: "+datumValute.get(GregorianCalendar.YEAR); 
+	}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	valuta other = (valuta) obj;
+	if (datumValute == null) {
+		if (other.datumValute != null)
+			return false;
+	} else if (!datumValute.equals(other.datumValute))
+		return false;
+	if (kupovni != other.kupovni)
+		return false;
+	if (naziv == null) {
+		if (other.naziv != null)
+			return false;
+	} else if (!naziv.equals(other.naziv))
+		return false;
+	if (prodajni != other.prodajni)
+		return false;
+	if (skraceniNaziv == null) {
+		if (other.skraceniNaziv != null)
+			return false;
+	} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+		return false;
+	if (srednji == null) {
+		if (other.srednji != null)
+			return false;
+	} else if (!srednji.equals(other.srednji))
+		return false;
+	return true;
+}
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result
+			+ ((datumValute == null) ? 0 : datumValute.hashCode());
+	result = prime * result + kupovni;
+	result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+	result = prime * result + prodajni;
+	result = prime * result
+			+ ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+	result = prime * result + ((srednji == null) ? 0 : srednji.hashCode());
+	return result;
+}
 
 }
